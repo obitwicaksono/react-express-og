7665e9f1cc57bf26bcf49987290192bee2190f63
obitwicaksono <ubit332@gmail.com>
2025-07-15 07:26:45 +0700

    change cors configuration on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,9 @@ const app = express();
 const usersRoutes = require("./routes/users");
 const middlewareLogRequest = require("./middleware/logs");
 const cors = require('cors');
-app.use(cors({
-  origin: ['https://react-express-production.up.railway.app', 'http://localhost:5173']
-}));
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['https://react-express-production.up.railway.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
