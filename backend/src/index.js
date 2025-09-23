6a9c901cb8dd8acf61ef936efb0ce24075b9dddf
obitwicaksono <ubit332@gmail.com>
2025-09-24 05:57:10 +0700

    index.js pada backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const middlewareLogRequest = require("./middleware/logs");
 const cors = require('cors');
 
 app.use(cors({
-  origin: ['https://react-express-production.up.railway.app', 'http://localhost:3000'],
+  origin: ['https://react-express-og.vercel.app/', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
